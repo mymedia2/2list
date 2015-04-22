d81e868980d7a18a663a39f16eba8ae4f5897c28
Guriev Nicholas <guriev-ns@ya.ru>
2015-04-22 21:13:03 +0300

    Описания функций в list.h

--- algorithm.c
+++ algorithm.c
@@ -1,11 +1,11 @@
 #include "list.h"
 
 void lst_for_each(list_t lst, lst_elem_t (*f)(lst_elem_t)) {
-	const size_t n = sizeof(lst->elems) / sizeof(*lst->elems);
 	size_t i;
 
+	/* итерируемся по списку, пропуская первое звено */
 	while (lst = lst->next) {
-		for (i = 0; i < n; i++) {
+		for (i = 0; i < lst->count; i++) {
 			lst->elems[i] = f(lst->elems[i]);
 		}
 	}
