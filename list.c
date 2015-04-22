6a968fee3251250b04b395cff770c2b4be1c1382
Guriev Nicholas <guriev-ns@ya.ru>
2015-04-22 15:24:07 +0300

    Добавлена lst_new

---
+++ list.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+#include "list.h"
+
+int lst_new(list_t* p) {
+	struct lst_node_* tmp;
+
+	tmp = malloc(sizeof(*tmp));
+	if (!tmp) {
+		/* не удалось выделить память — ошибка */
+		return 0;
+	}
+
+	*p = tmp;
+	return 1;
+}
