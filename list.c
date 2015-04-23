db024da142215b8c0c6f54466251262c07b7541d
Tabalin Dmitry <corovan@mail.ru>
2015-04-23 10:43:13 +0300

    Добавлена lst_insert_before, is_null для итератора

--- list.c
+++ list.c
@@ -45,14 +45,14 @@ lst_elem_t lst_index(list_t lst, size_t i) {
 lst_iter_t next(lst_iter_t t) {
     lst_iter_t l;
     l.offset = (t.offset + 1) % t.box->count;
-    if(l.offset == 0) l.box = t.box->next;
+    if (l.offset == 0) l.box = t.box->next;
     else l.box = t.box;
     return l;
 }
 
 lst_iter_t prev(lst_iter_t t) {
     lst_iter_t l;
-    if( t.offset != 0 ) {
+    if (t.offset != 0) {
         l.offset = t.offset - 1;
         l.box = t.box;
     } else {
@@ -60,4 +60,39 @@ lst_iter_t prev(lst_iter_t t) {
         l.offset = l.box->count;
     }
     return l;
+}
+
+int is_null( lst_iter_t t ) {
+    return t.box == NULL;
+}
+
+int lst_insert_before(lst_iter_t it, lst_elem_t el) {
+    const size_t n = sizeof(it.box->elems) / sizeof(lst_elem_t); 
+    if (it.box->count < n && it.offset != 0) {
+        /* массив не заполнен, сдвигаем другие элементы */
+        int i;
+        for (i = (it.box->count)++; i >= it.offset; --i) {
+            it.box->elems[i] = it.box->elems[i-1];
+        }
+        it.box->elems[it.offset] = el;
+    } else if(it.box->prev && it.box->prev->count < n){
+        /* массив в предыдущем звене не заполнен */
+        it.box->prev->elems[(it.box->prev->count)++] = el;
+    } else {
+        /* добавление нового звена */
+        /* TODO сделать, чтобы это не была копипаста из lst_new */
+        struct lst_node_* tmp;
+
+        tmp = malloc(sizeof(*tmp));
+        if (!tmp) {
+            /* не удалось выделить память — ошибка */
+            return 0;
+        }
+        
+        tmp->count = 0;
+        tmp->elems[tmp->count] = el;
+        tmp->prev = it.box->prev;
+        tmp->next = it.box;
+        it.box->prev = tmp;
+    }
 }
