7a50828b55c1b30d31f338f62e8b6dcee3c5ddf1
Дмитрий Руденко <dimitriy_rudenko@mail.ru>
2015-04-23 19:36:14 +0300

    Исправлена плавающая ссылка и все работает

--- list.c
+++ list.c
@@ -117,10 +117,10 @@ int lst_insert_before(lst_iter_t it, lst_elem_t el) {
 int lst_elem_count(list_t lst) {
     list_t counter_lst=lst;
     int counter;
-    while (counter_lst=counter_lst->next!==NULL) {
+    while ((counter_lst->next)!=NULL) {
+        counter_lst=counter_lst->next;
         counter++; //Считаем кол-во объектов с списке
     }
-    counter++; //считаем предполседний элемент
 
     counter*=10; ///У нас 10 элементов в объекте, так что считаем число counter*10;
     counter+=counter_lst->count; //Элементы в последнем звене;
